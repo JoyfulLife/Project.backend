dc43e7b5770b821371f65f494dee9ae05ecc081f
joysfulList <joys0719@gmail.com>
2022-04-15 01:58:30 +0900

    Initial commit

---
+++ src/main/java/project/backend/controller/ClientController.java
@@ -0,0 +1,29 @@
+package project.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import project.backend.vo.ClientVo;
+
+import java.util.Locale;
+
+@Controller
+public class ClientController {
+    @RequestMapping(value = "/ado/client/clientInfo", method = RequestMethod.GET)
+    @ResponseBody
+    public ClientVo getclient(Locale locale, @ModelAttribute ClientVo ClientVO) {
+//        ResponseVO responseVO = new ResponseVO();
+        ClientVo ClientVo = new ClientVo();
+//        try {
+//            retrieveUserInfoAndValidate(ADOClientVO);
+//
+//            responseVO = ADOClientService.selectClient(ADOClientVO);
+//        } catch (Exception exception) {
+//            responseVO = new ResponseVO();
+//            ExceptionUtils.handleException(exception, this, responseVO, locale);
+//        }
+        return ClientVo;
+    }
+}

32bd60e177a056f34e17e0fdf30017efbbe112eb
joys <joys0719@blue-core.com>
2022-04-21 16:27:46 +0900

    04-21 회원가입 변수 추가

--- src/main/java/project/backend/vo/ClientVo.java
+++ src/main/java/project/backend/vo/ClientVo.java
@@ -4,6 +4,9 @@ public class ClientVo {
     private String userID = "";
     private String passWord = "";
     private String reconfirmPassWord = "";
+    private String name = "";
+    private String phoneNumber = "";
+    private String gender = "";
 
     public String getUserID() {
         return userID;
@@ -22,5 +25,28 @@ public class ClientVo {
     }
     public void setReconfirmPassWord(String reconfirmPassWord) {
         this.reconfirmPassWord = reconfirmPassWord;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 }
